0dcd06a345bb29475bd072f7a7edf7694c6e70de
José Luis Cánovas Sánchez <joseluis.canovas@outlook.com>
2019-01-21 20:33:00 +0100

    parse and show basic info

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,48 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ViewColumn } from 'vscode';
+import { Certificate } from '@fidm/x509';
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "x509-parser" is now active!');
+	const x509PEMProvider = new class implements vscode.TextDocumentContentProvider {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		// emitter and its event
+		onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+		onDidChange = this.onDidChangeEmitter.event;
+		
+		provideTextDocumentContent(uri: vscode.Uri): string {
+			let certificate = Certificate.fromPEM(new Buffer(decodeURIComponent(uri.path))).toJSON();
+			let document = {
+				SerialNumber: certificate.serialNumber,
+				Subject: certificate.subject,
+				Issuer: certificate.issuer
+			}
+			return JSON.stringify(document, null, 4);
+		}
+	};
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider("x509PEM", x509PEMProvider));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('extension.parsePEM', async () => {
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		let document = editor.document;
+		let uri = vscode.Uri.parse('x509PEM:' + document.getText());
+		try{
+			let doc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
+			doc = await vscode.workspace.openTextDocument({language: 'json', content: doc.getText()});
+			await vscode.window.showTextDocument(doc, { preview: false, viewColumn: ViewColumn.Beside });
+		} catch(e) {
+			return;
+		}
+	}));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
