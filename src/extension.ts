72c17add0c86897424de132e77eff2a4f01b6c19
José Luis Cánovas <jlcs-es@users.noreply.github.com>
2021-03-12 20:35:34 +0100

    improve END CERT injection

--- src/extension.ts
+++ src/extension.ts
@@ -59,7 +59,12 @@ async function runAndShowOpenSSLCmd(command: string) {
 	let pem = editor.document.getText();
 	// Check if the required PEM header and footer exist, if not assume the document contains a certificate
 	if (!pem.startsWith('-----') && !pem.endsWith('-----')){
-		pem = '-----BEGIN CERTIFICATE-----\n' + pem + '\n-----END CERTIFICATE-----';
+		pem = '-----BEGIN CERTIFICATE-----\n' + pem;
+		if (pem.endsWith('\n')) {
+			pem += '-----END CERTIFICATE-----';
+		} else {
+			pem += '\n-----END CERTIFICATE-----';
+		}
 	}
 	let result = openssl.stdin(pem).cmd(command).exec();
 	let output = result.stdout;
